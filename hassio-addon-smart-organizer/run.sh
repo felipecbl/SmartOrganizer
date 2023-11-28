9861c346dfed951a3076b53826800fcd10c22cc0
Felipe <felipecb@me.com>
2023-11-27 17:52:22 -0800

    reverted all s

--- hassio-addon-smart-organizer/run.sh
+++ hassio-addon-smart-organizer/run.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/with-contenv bashio
-set +u
+#!/bin/bash
+set -e
 
+CONFIG_PATH=/data/options.json
+CONNECTION_STRING="$(jq --raw-output '.connectionString' $CONFIG_PATH)"
 
-bashio::log.info "Starting Smart Organizer."
-npm run start
+echo Hello!
+node -v
+npm -v
+npm install
+node index.js
