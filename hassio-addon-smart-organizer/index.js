0859f4b218fe566404ed82dc2414d771aa1c250e
Felipe <felipecb@me.com>
2023-11-27 21:15:16 -0800

    new config

--- hassio-addon-smart-organizer/index.js
+++ hassio-addon-smart-organizer/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 console.log('Hello World from Smart Organizer project');
 
