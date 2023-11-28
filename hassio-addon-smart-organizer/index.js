80537ea9b63994ff907f76b3968635e7e1952364
Felipe <felipecb@me.com>
2023-11-27 20:53:10 -0800

    test

--- hassio-addon-smart-organizer/index.js
+++ hassio-addon-smart-organizer/index.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const express = require('express');
+
 const app = express();
+const port = process.env.PORT || 8000;
 
 console.log('Hello World from Smart Organizer project');
 
-app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static(path.join(__dirname, "/index.html")));
 
 // start express server on port 8000
-app.listen(8000, () => {
-  console.log("server started on port 8000");
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
 });
